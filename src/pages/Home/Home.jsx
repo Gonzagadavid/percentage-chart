954c61dd0811b47a1d06aa62bed6956e198a4d5a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-09 10:56:16 -0300

    finalizado estilizacao ninicial

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,13 +9,14 @@ const Home = () => (
       Essa página tem como finalidade auxiliar na estilização do componente
       libary stylized-percentage-chart
     </p>
-    <p>Na página Estilizar gráfico encontrar ferramentas que permitirá que estilize o grafico:</p>
+    <p>Na página Estilizar gráfico encontrar ferramentas que permitirá que estilize o gráfico:</p>
     <ul>
-      <li>Na guia Backgroud, estilizar o fundo do seu gráfico</li>
-      <li>Na guia Inserir Dados, insira dados simulando o que ocorrerá na sua aplização</li>
-      <li>Na guia Editar Barras, poderá alterar e excluir os dados e trocar a cor das barras</li>
-      <li>Na guia Estilizar Barras, configure detalhes nos estilo das barras</li>
-      <li>Na guia Legenda, poderá optar por usar legenda estilizada a seu gosto</li>
+      <li>Na guia Backgroud, estilizar o fundo do seu gráfico;</li>
+      <li>Na guia Inserir Dados, insira dados simulando o que ocorrerá na sua aplização;</li>
+      <li>Na guia Editar Barras, poderá alterar e excluir os dados e trocar a cor das barras;</li>
+      <li>Na guia Estilizar Barras, configure detalhes nos estilo das barras;</li>
+      <li>Na guia Legenda, poderá optar por usar legenda estilizada a seu gosto;</li>
+      <p>Para visualizar as alterações clique em aplicar;</p>
     </ul>
     <Link to="/settings">Inicar</Link>
   </div>
